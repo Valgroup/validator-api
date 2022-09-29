258e4721c31f9573721091f24cc2f353899734b7
joao.recanello <joao.recanello_ext@valgroupco.com>
2022-09-29 09:59:14 -0300

    Removido cargo das pendencias

--- Validator-API/Validator.Domain/Commands/Planilhas/PlanilhaResolverPendenciaCommand.cs
+++ Validator-API/Validator.Domain/Commands/Planilhas/PlanilhaResolverPendenciaCommand.cs
@@ -7,7 +7,6 @@
         public string? CPF { get; set; }
         public string? Nome { get; set; }
         public string? Email { get; set; }
-        public string? Cargo { get; set; }
         public string? Nivel { get; set; }
         public string? SuperiorImediato { get; set; }
         public string? EmailSuperior { get; set; }
